9524a06e098d865b7f363b07bdc9e0a39482e2ba
Pathan Sharukh Khan <145139884+sharukh010@users.noreply.github.com>
2026-09-06 22:44:59 +0530

    Add files via upload

---
+++ Lecture-11/dictionary.py
@@ -0,0 +1,22 @@
+# student = {
+#     "name": "karthik", 
+#     "age": 14 , 
+#     "gender": "male",
+#     "dob": "19/06/2012",
+#     "grade": "year 10"
+# }
+student = {
+    "name": "karthik", 
+    "age": 14 , 
+    "gender": "male",
+    "dob": "19/06/2012",
+    "grade": "year 10",
+    "age":15,
+    # 15:"numeric key"
+}
+# student = {"name":"karthik","age":14,...}
+# print(student)
+# print(student["name"])
+# print(student["age"])
+# print(student[15])
+print(student["school"])
